eebb04df98ac66f997603a3bd6348749c158e618
Jack Will <65641460+jackwill99@users.noreply.github.com>
2023-11-01 11:51:07 +0630

    events methods bug fixed

--- lib/communication/communication.dart
+++ lib/communication/communication.dart
@@ -13,6 +13,17 @@ class Communication {
 
   late Room room;
 
+  /// Events methods
+  /// Events listen once while calling and function references miss when minimized call screen
+  /// So, store events methods
+  ///
+  FutureOr<void> Function()? _onRoomJoined;
+  FutureOr<void> Function(dynamic error)? _onRoomLeft;
+  FutureOr<void> Function(Stream stream)? _onStreamEnabled;
+  FutureOr<void> Function(Stream stream)? _onStreamDisabled;
+  FutureOr<void> Function(String id)? _onPresenterChanged;
+  FutureOr<void> Function()? _onParticipantLeft;
+
   /// Create instance of Room (Meeting)
   ///
   Room createRoom({
@@ -43,42 +54,49 @@ class Communication {
     FutureOr<void> Function(String id)? onPresenterChanged,
     FutureOr<void> Function()? onParticipantLeft,
   }) async {
+    _onRoomJoined = onRoomJoined;
+    _onRoomLeft = onRoomLeft;
+    _onStreamEnabled = onStreamEnabled;
+    _onStreamDisabled = onStreamDisabled;
+    _onPresenterChanged = onPresenterChanged;
+    _onParticipantLeft = onParticipantLeft;
+
     // Called when joined in meeting
     room
       ..on(
         Events.roomJoined,
         () {
-          onRoomJoined?.call();
+          _onRoomJoined?.call();
         },
       )
 
       // Called when meeting is ended
       ..on(Events.roomLeft, (errorMsg) {
-        onRoomLeft?.call(errorMsg);
+        _onRoomLeft?.call(errorMsg);
       });
 
     // Called when stream is enabled
     room.localParticipant.on(Events.streamEnabled, (stream) {
       final tempStream = stream as Stream;
-      onStreamEnabled?.call(tempStream);
+      _onStreamEnabled?.call(tempStream);
     });
 
     // Called when stream is disabled
     room.localParticipant.on(Events.streamDisabled, (stream) {
       final tempStream = stream as Stream;
 
-      onStreamDisabled?.call(tempStream);
+      _onStreamDisabled?.call(tempStream);
     });
 
     // Called when presenter is changed
     room
       ..on(Events.presenterChanged, (activePresenterId) {
-        onPresenterChanged?.call(activePresenterId.toString());
+        _onPresenterChanged?.call(activePresenterId.toString());
       })
       ..on(
         Events.participantLeft,
         (participant) {
-          onParticipantLeft?.call();
+          _onParticipantLeft?.call();
         },
       )
 
